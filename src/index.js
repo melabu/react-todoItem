b03644cbeef52a95412a13835f39710c767712c7
melabu <analia.drago@hotmail.com>
2022-01-06 11:34:23 -0300

    add wake data item

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import App from './App';
 const DATA = [
     { id: "todo-0", name: "Eat", completed: true },
     { id: "todo-1", name: "Sleep", completed: false },
-    { id: "todo-2", name: "Repeat", completed: false }
+    { id: "todo-2", name: "Repeat", completed: false },
+    { id: "todo-2", name: "Wake", completed: false }
   ];
 
 ReactDOM.render(<App tasks={DATA}/>, document.getElementById('root'));
